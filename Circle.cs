0847b84d6e6e610bd3001159ddfa95d98ab5fc78
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2024-10-17 09:33:28 +0200

    first commit

---
+++ Circle.cs
@@ -0,0 +1,22 @@
+﻿namespace Intro_till_OOP;
+
+public class Circle
+{
+    public double Radius { get; set; }
+
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+
+    public double GetSphereArea()
+    {
+        return 4 * Math.PI * Math.Pow(Radius, 3) / 3;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Intro_till_OOP
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Circle[] cirkels = [new Circle(5), new Circle(6)];
+
+            foreach (var circle in cirkels)
+            {
+                Console.WriteLine($"Aria: {circle.GetArea()}");
+                Console.WriteLine($"Sfär: {circle.GetSphereArea()}");
+            }
+        }
+    }
+}
